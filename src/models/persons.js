93fda6e82f95ad3df9ec6893538f4745f0765bdd
bsalin39 <bsalin39@stu.southtexascollege.edu>
2022-11-04 14:37:35 -0500

    Added missing returns

--- src/models/persons.js
+++ src/models/persons.js
@@ -1,16 +1,14 @@
 import { v4 as uuid } from 'uuid'
 
 const persons = []
-
 export const getPersons = () => persons
-
 export const getPerson = (id) => {
   return persons.find((person) => person.id === id)
 }
-
 export const createPerson = (person) => {
   const id = uuid()
   persons.push({ id, ...person })
+  return getPerson(id)
 }
 
 export const updatePerson = (id, person) => {
@@ -19,11 +17,14 @@ export const updatePerson = (id, person) => {
     const personIndex = persons.findIndex((p) => p.id === id)
     persons[personIndex] = { id, ...person }
   }
+  return getPerson(id)
 }
 
 export const deletePerson = (id) => {
   const personIndex = persons.findIndex((p) => p.id === id)
   if (personIndex !== -1) {
     persons.splice(personIndex, 1)
+    return true
   }
+  return false
 }
