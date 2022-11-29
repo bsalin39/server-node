39b6a4915b6b6ac3ac4a9b9395956c994ef7580f
bsalin39 <bsalin39@stu.southtexascollege.edu>
2022-11-29 15:22:50 -0600

    added department creation fr this time

--- src/routes/api/companies.js
+++ src/routes/api/companies.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 import { getCompanies, getCompany, addCompany } from '../../models/companies'
+import { addDepartment } from '../../models/departments'
 
 const router = Router()
 
@@ -8,7 +9,6 @@ router.get('/', async (req, res) => {
   const companies = await getCompanies()
   res.send(companies)
 })
-
 router.get('/:id', async (req, res) => {
   const company = await getCompany(req.params.id)
   if (company) {
@@ -17,7 +17,6 @@ router.get('/:id', async (req, res) => {
     res.status(404).send({ msg: 'Company not found' })
   }
 })
-
 router.post('/', async (req, res) => {
   const companyName = req.body.name
   if (companyName) {
@@ -25,6 +24,22 @@ router.post('/', async (req, res) => {
     res.send(company)
   } else {
     res.status(400).send({ msg: 'Company name is required' })
+  }
+})
+
+router.post('/:id', async (req, res) => {
+  const companyId = req.params.id
+  const company = await getCompany(companyId)
+  if (company) {
+    const departmentName = req.body.name
+    if (departmentName) {
+      const department = await addDepartment(departmentName, companyId)
+      res.send(department)
+    } else {
+      res.status(400).send({ msg: 'Department name is required' })
+    }
+  } else {
+    res.status(400).send({ msg: 'Company does not exist required' })
   }
 })
 
