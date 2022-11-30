c6789d3e0abfb639d41ccefbffbfc0113a471010
bsalin39 <bsalin39@stu.southtexascollege.edu>
2022-11-29 22:44:53 -0600

    added employee endpoint

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,17 +1,17 @@
 import { Router } from 'express'
-
 import persons from './persons'
 import companies from './companies'
 import departments from './departments'
+import employees from './employees'
 
 const router = Router()
 
 router.get('/', (req, res) => {
   res.send({ msg: 'Inside API Endpoints' })
 })
-
 router.use('/persons', persons)
 router.use('/companies', companies)
 router.use('/departments', departments)
+router.use('/employees', employees)
 
 export default router
